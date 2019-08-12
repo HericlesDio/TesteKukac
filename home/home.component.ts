f13de9b8e856f84fbc4874ff1b53cd7decf732c8
Hericles <32101681+HericlesDio@users.noreply.github.com>
2019-08-11 23:44:31 -0300

    Update home.component.ts

--- home/home.component.ts
+++ home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Endereco } from '../model/cep.model';
-import { cepService } from './cep.service';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -10,60 +10,49 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
 
-  resultado:string;
+  resultado: string;
 
-  nometitular: string; 
+  enderecoTxt: string;
+  nometitular: string;
   cep: Endereco;
-  renda:number;
+  renda: number;
 
-  constructor(private cepService:cepService, private http: HttpClient) { 
+  constructor(private http: HttpClient) {
   }
 
   ngOnInit() {
   }
 
-  pegarCep(cep:string){
-    console.log(cep);
-    return  this.http.get(`viacep.com.br/ws/${cep}/json/ `)
-    .subscribe(data => this.cep = this.conversaoCep(data));
+  pegarCep(cep: string, nome: string, rendaCap: number) {
+    
+    this.http.get(`https://viacep.com.br/ws/${cep}/json/`)
+    .pipe(map(res => res))
+    .subscribe(res => {
+       this.enderecoTxt = res['logradouro'] + ', ' + res['bairro'] + ', ' + res['localidade'] + ' - ' + res['uf'] + ', ibge ' + res['ibge'];
+       this.abrirInfo(nome, this.enderecoTxt, rendaCap);
+    });
   }
 
-  conversaoCep(cepResposta):Endereco{
-    console.log(cepResposta);
-    let cp : Endereco;
-     cp.bairro = cepResposta.bairro;
-     cp.cep = cepResposta.cep;
-     cp.complemento = cepResposta.complemento;
-     cp.gia = cepResposta.gia;
-     cp.ibge = cepResposta.ibge;
-     cp.localidade = cepResposta.localidade;
-     cp.logradouro = cepResposta.logradouro;
-     cp.uf = cepResposta.uf;
-     cp.unidade = cepResposta.unidade;
-    return cp;
+
+  getrenda(dinheiro: number, dependentes: number) {
+    return dinheiro / dependentes;
   }
 
-  getrenda(dinheiro:number, dependentes:number){
-    return dinheiro/dependentes;
-  }
+  mensagem(nometit: string = "sem nome", cp: string, dinheiro: number, dependentes: number) {
 
-  mensagem(nometit:string="sem nome",cp:string,dinheiro:string, dependentes:string ) {
-    this.renda = this.getrenda(dinheiro, dependentes);
-    this.pegarCep(cp);
-    console.log(this.cep);
-    //fala que vai dar errado mas funciona
-    if(cp == "" || dinheiro == "" || dependentes == "" ){
-      alert("Por Favor Digite os valores nos campos Cep, Renda Mensal e Número de dependentes")
-    }else if(cp.length < 8 || cp.length >9){
+    if (cp == "" || dinheiro == null || dependentes == null) {
+      alert("Por favor digite os valores nos campos cep, renda mensal e número de dependentes")
+    } else if (cp.length != 8) {
       console.log(cp.length);
-      alert("Cep não está do tamanho certo");
-    }else {
-    alert(" Senhor "+nometit+"\n o senhor mora em"+this.cep.bairro+"\n"+this.cep.cep+"\n"+this.cep.complemento+"\n"+this.cep.gia+
-    "\n"+this.cep.ibge+"\n"+this.cep.localidade+"\n"+this.cep.logradouro+
-    "\n"+this.cep.uf+"\n"+this.cep.unidade+"\n e sua renda per capta é de:"+this.renda);
+      alert("Cep incorreto");
+    } else {
+      this.renda = this.getrenda(dinheiro, dependentes);
+      this.pegarCep(cp, nometit, this.renda);
+    }
   }
-}
 
- 
+  abrirInfo(nome: string, endereco: string, renda:number) {
+    alert("Senhor " + nome + ", seu endereço é " + endereco + " e sua renda per capita é de: " + renda);
+  }
 
 }
